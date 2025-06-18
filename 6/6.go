5326caa4b38e92fcaf8b1e57cd859ba4df388462
z1rden <arseniykulagin@yandex.ru>
2025-06-18 15:30:10 +0300

    Добавил задачу

---
+++ 6/6.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(convert("PAYPALISHIRING", 4))
+	fmt.Println(convert("AB", 1))
+}
+
+/*
+Суть алгоритма в двойном указателе:
+1. Поочередно считываем символы;
+2. Второй указатель в это время работает по следующему правилу:
+  - если он достиг numrows - 1 (верхней границы записи), то идем в обратную сторону ( 3 2 1 )
+  - если он достиг 0 (нижней границы записи), то идем в обратную сторону (1 2 3)
+
+Временная сложность: O(2).
+Пространственная сложность: O(1).
+*/
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+
+	rows := make([][]byte, numRows)
+	j := 0
+	d := 1
+	for i := 0; i < len(s); i++ {
+		rows[j] = append(rows[j], s[i])
+
+		if j == numRows-1 {
+			d = -1
+		} else if j == 0 {
+			d = 1
+		}
+
+		j += d
+	}
+
+	var result []byte
+	for i := 0; i < numRows; i++ {
+		result = append(result, rows[i]...)
+	}
+
+	return string(result)
+}
---
+++ 6/README.md
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/longest-palindromic-substring/description/?source=submission-noac
+# Самая длинная палиндромная подстрока
+Строка "PAYPALISHIRING" записывается в виде зигзага в заданном количестве строк следующим образом: (для лучшей читаемости вы можете отобразить этот узор фиксированным шрифтом).
+Затем читайте построчно: "PAHNAPLSIIGYIR".
+Напишите код, который будет принимать строку и выполнять это преобразование для заданного количества строк: func convert(s string, numRows int) string
+
+## Пример 1:
+Ввод: s = «PAYPALISHIRING», numRows = 3
+Вывод: «PAHNAPLSIIGYIR»
+
+## Пример 2:
+Ввод: s = «PAYPALISHIRING», количество строк = 4
+Вывод: «PINALSIGYAHRPI»
+
+## Пример 3:
+Входные данные: s = «A», numRows = 1
+Вывод: «A»
